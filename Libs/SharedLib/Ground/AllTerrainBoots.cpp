17f90073da27b61baa6ec0ff49ed0c6556debef0
Yuri Solyanik <jura.s.n@gmail.com>
2024-10-29 15:11:24 +0500

    Добавил проверку последней остановки для наземных ТС

--- Libs/SharedLib/Ground/AllTerrainBoots.cpp
+++ Libs/SharedLib/Ground/AllTerrainBoots.cpp
@@ -13,6 +13,12 @@ double AllTerrainBoots::calculateTotalTime(int distance) {
     // Количество перерывов
     int breaks = static_cast<int>(totalTime / m_movementTime);
 
+    // Проверяем, нужно ли делать последний отдых или путь будет пройден до конца
+    if (static_cast<int>(totalTime) % m_movementTime == 0) {
+        // Остатка нет, значит последний отдых не требуется
+        breaks--;
+    }
+
     // Добавление времени отдыха
     for (int i{0}; i < breaks; ++i) {
         if (i == 0) {
--- Libs/SharedLib/Ground/Centaur.cpp
+++ Libs/SharedLib/Ground/Centaur.cpp
@@ -11,6 +11,12 @@ double Centaur::calculateTotalTime(int distance) {
     // Количество перерывов
     int breaks = static_cast<int>(totalTime / m_movementTime);
 
+    // Проверяем, нужно ли делать последний отдых или путь будет пройден до конца
+    if (static_cast<int>(totalTime) % m_movementTime == 0) {
+        // Остатка нет, значит последний отдых не требуется
+        breaks--;
+    }
+
     // Добавление времени отдыха
     for (int i{0}; i < breaks; ++i) {
         totalTime += restDuration;
